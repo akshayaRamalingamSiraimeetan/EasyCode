56a163b4e34aab357ddcd154c70363f11ec0ffc3
akshayaRamalingamSiraimeetan <akshayaramalingamsiraimeetan@gmail.com>
2026-07-01 21:19:18 +0530

    feat : jwt authentication middleware added

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -130,7 +130,33 @@ const login = async (req, res) => {
   }
 };
 
+const getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.user.id }).select("-passwordHash");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+    });
+  }
+};
+
 module.exports = {
   register,
   login,
+  getCurrentUser,
 };
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const { register, login } = require("../controllers/authController");
-
+const { register, login, getCurrentUser } = require("../controllers/authController");
 const router = express.Router();
+const authenticate = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/me",authenticate,getCurrentUser);
 module.exports = router;
