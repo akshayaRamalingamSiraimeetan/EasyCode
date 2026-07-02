ecacf686e19044d121d75fa8522a49da6b840fa6
akshayaRamalingamSiraimeetan <akshayaramalingamsiraimeetan@gmail.com>
2026-07-02 22:00:47 +0530

    style: changed problem table icons and difficulty badges

--- client/src/components/ProblemRow.jsx
+++ client/src/components/ProblemRow.jsx
@@ -1,4 +1,22 @@
 import { useAuth } from "../context/AuthContext";
+import { FiEdit2, FiTrash2 } from "react-icons/fi";
+
+const difficultyConfig = {
+  Easy: {
+    label: "Easy",
+    className: "easy",
+  },
+
+  Medium: {
+    label: "Medium",
+    className: "medium",
+  },
+
+  Hard: {
+    label: "Hard",
+    className: "hard",
+  },
+};
 
 function ProblemRow({ index, problem, onEdit, onDelete }) {
   const { user } = useAuth();
@@ -10,8 +28,12 @@ function ProblemRow({ index, problem, onEdit, onDelete }) {
       <td>{problem.title}</td>
 
       <td>
-        <span className={`difficulty ${problem.difficulty.toLowerCase()}`}>
-          {problem.difficulty}
+        <span
+          className={`difficulty ${
+            difficultyConfig[problem.difficulty].className
+          }`}
+        >
+          {difficultyConfig[problem.difficulty].label}
         </span>
       </td>
 
@@ -26,7 +48,7 @@ function ProblemRow({ index, problem, onEdit, onDelete }) {
                 onClick={() => onEdit(problem)}
                 title="Edit Problem"
               >
-                ✏
+                <FiEdit2 />
               </button>
 
               <button
@@ -34,7 +56,7 @@ function ProblemRow({ index, problem, onEdit, onDelete }) {
                 onClick={() => onDelete(problem)}
                 title="Delete Problem"
               >
-                🗑
+                <FiTrash2 />
               </button>
             </>
           )}
