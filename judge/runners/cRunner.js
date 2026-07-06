b2b70cea32f7a1afd69eae3bc27dbf98ad2a1482
akshayaRamalingamSiraimeetan <akshayaramalingamsiraimeetan@gmail.com>
2026-07-06 19:46:13 +0530

    feat: compile C source using gcc

--- judge/runners/cRunner.js
+++ judge/runners/cRunner.js
@@ -1,17 +1,35 @@
 const fs = require("fs");
 const path = require("path");
 const { v4: uuid } = require("uuid");
+const { spawn } = require("child_process");
 
 async function execute(code, input) {
   return new Promise((resolve, reject) => {
     const fileName = `${uuid()}.c`;
     const filePath = path.join(__dirname, "..", "temp", fileName);
+    const executableName = `${uuid()}.exe`;
+    const executablePath = path.join(__dirname, "..", "temp", executableName);
 
     fs.writeFileSync(filePath, code);
 
-    resolve({
-      stdout: "C source file created",
-      stderr: "",
+    const gccProcess = spawn("gcc", [filePath, "-o", executablePath]);
+
+    let compileError = "";
+    gccProcess.stderr.on("data", (data) => {
+      compileError += data.toString();
+    });
+
+    gccProcess.on("close", (code) => {
+      if (code !== 0) {
+        console.log("GCC exit code:", code);
+        console.log("Compiler stderr:", compileError);
+
+        return reject(new Error(compileError));
+        resolve({
+          stdout: "Compilation successful",
+          stderr: "",
+        });
+      }
     });
   });
 }
