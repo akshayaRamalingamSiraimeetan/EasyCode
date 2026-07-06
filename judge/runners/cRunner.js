960861ff694fb6e49fbf483b8ad30a26b02140fb
akshayaRamalingamSiraimeetan <akshayaramalingamsiraimeetan@gmail.com>
2026-07-06 19:35:00 +0530

    feat: write C source file

--- judge/runners/cRunner.js
+++ judge/runners/cRunner.js
@@ -1,7 +1,21 @@
-async function execute(code, input) {
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
 
+async function execute(code, input) {
+  return new Promise((resolve, reject) => {
+    const fileName = `${uuid()}.c`;
+    const filePath = path.join(__dirname, "..", "temp", fileName);
+
+    fs.writeFileSync(filePath, code);
+
+    resolve({
+      stdout: "C source file created",
+      stderr: "",
+    });
+  });
 }
 
 module.exports = {
-    execute,
+  execute,
 };
