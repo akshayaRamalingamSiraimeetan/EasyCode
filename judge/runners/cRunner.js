ce14b440467130b3d0eb4a06a3b952c5b20a517d
akshayaRamalingamSiraimeetan <akshayaramalingamsiraimeetan@gmail.com>
2026-07-06 21:11:02 +0530

    refactor: implement compiled language runner

--- judge/runners/cRunner.js
+++ judge/runners/cRunner.js
@@ -1,77 +1,12 @@
-const fs = require("fs");
-const path = require("path");
-const { v4: uuid } = require("uuid");
-const { spawn } = require("child_process");
-
-function deleteFile(filePath) {
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      console.error("Failed to delete:", filePath);
-    }
-  });
-}
+const compiledRunner = require("./compiledRunner");
 
 async function execute(code, input) {
-  return new Promise((resolve, reject) => {
-    const fileName = `${uuid()}.c`;
-    const filePath = path.join(__dirname, "..", "temp", fileName);
-    const executableName = `${uuid()}.exe`;
-    const executablePath = path.join(__dirname, "..", "temp", executableName);
-
-    fs.writeFileSync(filePath, code);
-
-    const gccProcess = spawn("gcc", [filePath, "-o", executablePath]);
-
-    let compileError = "";
-    gccProcess.stderr.on("data", (data) => {
-      compileError += data.toString();
-    });
-
-    gccProcess.on("close", (code) => {
-      if (code !== 0) {
-        console.log("GCC exit code:", code);
-        console.log("Compiler stderr:", compileError);
-        deleteFile(filePath);
-        return reject(new Error(compileError));
-      }
-      const programProcess = spawn(executablePath);
-
-      let stdout = "";
-      programProcess.stdout.on("data", (data) => {
-        stdout += data.toString();
-      });
-
-      let stderr = "";
-      programProcess.stderr.on("data", (data) => {
-        stderr += data.toString();
-      });
-
-      programProcess.stdin.write(input || "");
-      programProcess.stdin.end();
-
-      let timedOut = false;
-
-      const timeout = setTimeout(() => {
-        timedOut = true;
-        programProcess.kill();
-      }, 2000);
-
-      programProcess.on("close", () => {
-        clearTimeout(timeout);
-
-        if (timedOut) {
-          deleteFile(filePath);
-          deleteFile(executablePath);
-          return reject(new Error("Execution timed out"));
-        }
-        
-        resolve({
-          stdout,
-          stderr,
-        });
-      });
-    });
-  });
+  return compiledRunner.execute(
+    code,
+    input,
+    "c",
+    "gcc"
+  );
 }
 
 module.exports = {
