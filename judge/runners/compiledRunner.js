33fae37f3f5a705401268330bdab0ee8df48ec69
akshayaRamalingamSiraimeetan <akshayaramalingamsiraimeetan@gmail.com>
2026-07-14 19:00:08 +0530

    renamed dockerRunner to dockerExecutor

--- judge/runners/compiledRunner.js
+++ judge/runners/compiledRunner.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const { v4: uuid } = require("uuid");
-const { runInDocker } = require("../dockerRunner");
+const { runInDocker } = require("../dockerExecutor");
 
 const TEMP_DIR = path.join(__dirname, "..", "temp");
 
