ee1881707ab7eef410e0b9fd0af92c873b7439ce
akshayaRamalingamSiraimeetan <akshayaramalingamsiraimeetan@gmail.com>
2026-08-09 11:18:43 +0530

    Improve judge workspace setup and error logging

--- judge/src/middleware/errorMiddleware.js
+++ judge/src/middleware/errorMiddleware.js
@@ -7,6 +7,7 @@
 // eslint-disable-next-line no-unused-vars
 function errorMiddleware(err, req, res, next) {
   console.error("[Judge API] Unhandled error:", err.message);
+  console.error("[Judge API] Stack:", err.stack);
 
   const status = err.status ?? 500;
   res.status(status).json({
